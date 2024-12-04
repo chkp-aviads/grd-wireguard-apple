ce076491dd07370c519f69f320d8e936fa2d7a0d
Aviad Segev <aviads@checkpoint.com>
2024-12-04 12:25:23 +0200

    Fixed proxy testing code Added proper closing and waiting afterwards so we could check that the proxy server is really closed Updated wireproxy to log requests to the same logger and to properly close the proxy server

--- Sources/WireGuardKitGo/api-apple_test.go
+++ Sources/WireGuardKitGo/api-apple_test.go
@@ -8,8 +8,18 @@ import (
 )
 
 func TestWireGuardViaHttpProxy(t *testing.T) {
-	const saseConfig = `enter wireguard ini config here`
-	
+	const saseConfig = `
+[Interface]
+Address = 10.255.240.32
+DNS = 10.255.240.1
+PrivateKey = gGnCwagYqJMTWD1cPuqD4JobgXa0128U+/whiP86UGU=
+ListenPort = 8000
+
+[Peer]\nAllowedIPs = 10.255.240.0/24, 0.0.0.0/0
+PublicKey = 8o54P/m42zYYkMEhyTevws+/y7LNmBpTxXE8VyyDd0c=
+Endpoint = 67.55.94.84:8055
+`
+
 	username := "test"
 	password := "test"
 
@@ -23,22 +33,25 @@ func TestWireGuardViaHttpProxy(t *testing.T) {
 		t.Fatalf("Failed to parse proxy URL: %v", err)
 	}
 
+	// Add basic auth to the request using "Proxy-Authorization" header
+	auth := username + ":" + password
+	proxyAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+
 	transport := &http.Transport{
 		Proxy: http.ProxyURL(proxyURL),
+		ProxyConnectHeader: http.Header{
+			"Proxy-Authorization": []string{proxyAuth},
+		},
 	}
 
 	client := &http.Client{
 		Transport: transport,
 	}
 
-	req, err := http.NewRequest("GET", "http://example.com/", nil)
+	req, err := http.NewRequest("GET", "https://example.com/", nil)
 	if err != nil {
 		t.Fatalf("Failed to create request: %v", err)
 	}
-
-	// Add basic auth to the request using "Proxy-Authorization" header
-	auth := username + ":" + password
-	req.Header.Add("Proxy-Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(auth)))
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -49,4 +62,9 @@ func TestWireGuardViaHttpProxy(t *testing.T) {
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("Expected status code 200, got %d", resp.StatusCode)
 	}
+
+	wgTurnOff(handle)
+	t.Logf("WireGuard proxy turned off")
+	// wait indefinitely
+	select {}
 }
